b30cb8cda17ac16537eb17f62b22b92f73fa36aa
Saad Elnady <saad.elnady.work@gmail.com>
2023-03-31 22:18:06 +0200

    clean code and validate form

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 let btnScrollTotop = document.getElementsByClassName("scrollTotop")[0];
 
 onscroll = () => {
-    if (scrollY >= 200) {
-        btnScrollTotop.style.display = "block";
-    } else {
-        btnScrollTotop.style.display = "none";
-    }
+  if (scrollY >= 200) {
+    btnScrollTotop.style.display = "block";
+  } else {
+    btnScrollTotop.style.display = "none";
+  }
 };
 btnScrollTotop.onclick = () => {
-    scroll({ top: 0, behavior: "smooth" });
+  scroll({ top: 0, behavior: "smooth" });
 };
 // overlay
 let formDiv = document.querySelector(".overlay");
@@ -29,63 +29,72 @@ let tableBody = document.getElementById("tbody");
 // create user
 let users;
 if (localStorage.users != null) {
-    users = JSON.parse(localStorage.getItem("users"));
+  users = JSON.parse(localStorage.getItem("users"));
 } else {
-    users = [];
+  users = [];
 }
 let mood = "create";
 // using temp for edit  table row
 // temp = i
 let temp;
-save.onclick = function(e) {
-    e.preventDefault();
-    let user = {
-        name: name.value.toLowerCase(),
-        phone: phone.value,
-        email: email.value,
-        address: address.value.toLowerCase(),
-    };
-    // add object to array
+save.onclick = function (e) {
+  e.preventDefault();
+  let user = {
+    name: name.value.toLowerCase(),
+    phone: phone.value,
+    email: email.value,
+    address: address.value.toLowerCase(),
+  };
 
-    if (mood === "create") {
-        users.push(user);
-    } else {
-        // edit table row
-        // temp = i
-        users[temp] = user;
-        mood = "create";
-        save.value = "create";
-    }
-    // add users (array) to localStorage
-    localStorage.setItem("users", JSON.stringify(users));
-
-    clearForminputs();
-
+  if (
+    name.value != "" &&
+    phone.value != "" &&
+    address.value != "" &&
+    email.value != ""
+  ) {
     closeForm();
+    if (mood === "create") {
+      // add object to array
+      users.push(user);
+    } else {
+      // edit table row
+      // temp = i
+      users[temp] = user;
+      mood = "create";
+      save.value = "create";
+    }
+  } else {
+    return false;
+  }
 
-    showData();
+  // add users (array) to localStorage
+  localStorage.setItem("users", JSON.stringify(users));
+
+  clearForminputs();
+
+  showData();
 };
 
 function closeForm() {
-    formDiv.classList.remove("active");
+  formDiv.classList.remove("active");
 }
 
 function openForm() {
-    formDiv.classList.add("active");
+  formDiv.classList.add("active");
 }
 // remove form inputs
 function clearForminputs() {
-    name.value = "";
-    phone.value = "";
-    email.value = "";
-    address.value = "";
+  name.value = "";
+  phone.value = "";
+  email.value = "";
+  address.value = "";
 }
 
 function showData() {
-    let tableRow = "";
+  let tableRow = "";
 
-    for (let i = 0; i < users.length; i++) {
-        tableRow += `
+  for (let i = 0; i < users.length; i++) {
+    tableRow += `
     <tr>
         <td>${i + 1} </td>
         <td> ${users[i].name}  </td>
@@ -95,45 +104,45 @@ function showData() {
         <td> <button onclick = "editUser(${i})" id= "edit">Edit</button></td>
         <td> <button onclick = "deleteUser(${i})" id="delete">Delete</button></td>
       </tr>`;
-    }
-    tableBody.innerHTML = tableRow;
-    //create delete All btn
-    let deleteAlldiv = document.getElementById("deletAll");
+  }
+  tableBody.innerHTML = tableRow;
+  //create delete All btn
+  let deleteAlldiv = document.getElementById("deletAll");
 
-    if (users.length > 0) {
-        deleteAlldiv.innerHTML = `<button onclick ="deleteAll()" >Delete All (${users.length})</button>`;
-    } else {
-        deleteAlldiv.innerHTML = "";
-    }
+  if (users.length > 0) {
+    deleteAlldiv.innerHTML = `<button onclick ="deleteAll()" >Delete All (${users.length})</button>`;
+  } else {
+    deleteAlldiv.innerHTML = "";
+  }
 }
 showData();
 
 function deleteUser(i) {
-    // remove From array
-    users.splice(i, 1);
-    //Add new  array to localstorage after delete selected user
-    //  عنصر منها تعديل للمصفوفه اللى فى  اللوكال ستوردج بالمصفوفه الجديده بعد حذف
-    localStorage.users = JSON.stringify(users);
-    // trigger function show data
-    // هيشثغل التكرار على المصفوفه الجديده
-    showData();
+  // remove From array
+  users.splice(i, 1);
+  //Add new  array to localstorage after delete selected user
+  //  عنصر منها تعديل للمصفوفه اللى فى  اللوكال ستوردج بالمصفوفه الجديده بعد حذف
+  localStorage.users = JSON.stringify(users);
+  // trigger function show data
+  // هيشثغل التكرار على المصفوفه الجديده
+  showData();
 }
 
 function deleteAll() {
-    users.splice(0);
-    localStorage.clear();
-    showData();
+  users.splice(0);
+  localStorage.clear();
+  showData();
 }
 
 function editUser(i) {
-    openForm();
-    name.value = users[i].name;
-    phone.value = users[i].phone;
-    email.value = users[i].email;
-    address.value = users[i].address;
-    save.value = "Edit";
-    mood = "Edit";
-    temp = i;
+  openForm();
+  name.value = users[i].name;
+  phone.value = users[i].phone;
+  email.value = users[i].email;
+  address.value = users[i].address;
+  save.value = "Edit";
+  mood = "Edit";
+  temp = i;
 }
 
 // search input
@@ -143,38 +152,40 @@ let searchOptions = document.querySelectorAll(".searchby li");
 let searchInput = document.getElementById("search");
 
 function showSearchoption() {
-    searchArrow.classList.toggle("active");
-    searchBy.classList.toggle("active");
+  searchArrow.classList.toggle("active");
+  searchBy.classList.toggle("active");
 }
 
 searchArrow.onclick = showSearchoption;
 searchInput.onclick = showSearchoption;
 // searchInput.onblur = () => {
-//     searchArrow.classList.remove("active");
-//     searchBy.classList.remove("active");
+//   searchArrow.classList.remove("active");
+//   searchBy.classList.remove("active");
 // };
 let searchMood = "name";
 searchOptions.forEach((option) => {
-    option.onclick = () => {
-        searchArrow.classList.remove("active");
-        searchBy.classList.remove("active");
-        searchInput.placeholder = option.innerHTML;
-        if (option.id == "searchByname") {
-            searchMood = "name";
-        } else {
-            searchMood = "phone";
-        }
-        searchInput.focus();
-    };
+  option.onclick = () => {
+    searchArrow.classList.remove("active");
+    searchBy.classList.remove("active");
+    if (option.id == "searchByname") {
+      searchMood = "name";
+    } else {
+      searchMood = "phone";
+    }
+    searchInput.placeholder = option.innerHTML;
+    searchInput.focus();
+    searchInput.value = "";
+    showData();
+  };
 });
 searchInput.onkeyup = searchData;
 
 function searchData(value) {
-    let tableRow = "";
+  let tableRow = "";
+  for (let i = 0; i < users.length; i++) {
     if (searchMood === "name") {
-        for (let i = 0; i < users.length; i++) {
-            if (users[i].name.includes(this.value.toLowerCase())) {
-                tableRow += `
+      if (users[i].name.includes(this.value.toLowerCase())) {
+        tableRow += `
                     <tr>
                         <td>${i + 1} </td>
                         <td> ${users[i].name}  </td>
@@ -185,12 +196,10 @@ function searchData(value) {
                         <td> <button onclick = "deleteUser(${i})" id="delete">Delete</button></td>
                      </tr>
                    `;
-            }
-        }
+      }
     } else {
-        for (let i = 0; i < users.length; i++) {
-            if (users[i].phone.includes(this.value.toLowerCase())) {
-                tableRow += `
+      if (users[i].phone.includes(this.value.toLowerCase())) {
+        tableRow += `
                 <tr>
                     <td>${i + 1} </td>
                     <td> ${users[i].name}  </td>
@@ -201,9 +210,9 @@ function searchData(value) {
                     <td> <button onclick = "deleteUser(${i})" id="delete">Delete</button></td>
                  </tr>
                `;
-            }
-        }
+      }
     }
+  }
 
-    tableBody.innerHTML = tableRow;
+  tableBody.innerHTML = tableRow;
 }
